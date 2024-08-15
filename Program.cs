53ced5a3a9586474a4bfe9192be9e23711baf87c
Jeff Balbalosa <cyjeffb@yahoo.com>
2024-08-15 14:54:43 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.WriteLine("Average Calculator");
+
+int currentScore = 0;
+int sum = 0;
+int counter = 0;
+
+do
+{
+    Console.WriteLine("Enter your student score. Enter -1 to finish!");
+    currentScore = Console.
+    sum = sum + currentScore;
+    counter++;
+} while (currentScore < 0);
+
+int average = sum / counter;
+Console.WriteLine($"Average is {average}");
+
+Console.ReadKey();
